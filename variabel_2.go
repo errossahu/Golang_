aee13b0682219f652c1217e3acbc61513e42b9da
Yohanes Erros Sahu <duragonprojek@gmail.com>
2021-03-01 02:39:51 +0800

    Variabe

---
+++ variabel_2.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Yohanes Erro Sahu"
+	var age int = 32
+
+	fmt.Println(age)
+
+	fmt.Println(name)
+	var (
+		firstName = "Sahu"
+		lastName  = "Erros"
+	)
+	fmt.Println(firstName, lastName)
+}
